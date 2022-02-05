43008e8ba77c6a8d261fc4bb45a7ab044d1e04b5
Jackson Merma <jmermap@unsa.edu.pe>
2022-02-05 09:47:52 -0500

    Implementacion generica de Par

---
+++ ejemplos/genericidad/Par.java
@@ -0,0 +1,18 @@
+/**
+ * @author      : Jackson Merma (mermaportocarreroj@gmail.com)
+ * @created     : 05/02/2022
+ * @filename    : Par
+ */
+class Par <P>{
+	private P a, b;
+
+	public Par(P a, P b){
+		this.a = a;
+		this.b = b;
+	}
+	public Par<P> swap(){
+		return new Par<P>(b,a);
+	}
+}
+
+
