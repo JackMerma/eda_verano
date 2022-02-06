a10025ae1b6fa6a24fce074921afb6d7d0965ff7
Jackson Merma <jmermap@unsa.edu.pe>
2022-02-06 09:45:38 -0500

    plantilla linkedlist

---
+++ ejemplos/listas/LinkedList.java
@@ -0,0 +1,42 @@
+/**
+ * @author      : Jackson Merma (mermaportocarreroj@gmail.com)
+ * @created     : 06/02/2022
+ * @filename    : LinkedList
+ */
+class LinkedList {
+	class Node{
+		String data;
+		Node next;
+	}
+
+	public Node head;
+	public int cant = 0;
+
+	public boolean isEmpty(){
+		return false;
+	}
+
+	public void put(String dat){
+
+	}
+
+	public void add(String dat, int index){
+		//implementar
+	}
+	
+	public String search(String dat){
+		return "";
+	}
+
+	public String searchK(int index){
+		//implementar
+		return "";
+	}
+
+	public void delete(String dat){
+	
+	}
+
+}
+
+
