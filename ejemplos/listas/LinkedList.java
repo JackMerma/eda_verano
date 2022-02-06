17af8bbd0f3af47b8567ff65df58eb4941fc0b61
Jackson Merma <jmermap@unsa.edu.pe>
2022-02-06 09:57:01 -0500

    toString, empty, put...implementados

--- ejemplos/listas/LinkedList.java
+++ ejemplos/listas/LinkedList.java
@@ -7,17 +7,27 @@ class LinkedList {
 	class Node{
 		String data;
 		Node next;
+
+		public Node(String dat, Node next){
+			this.data = dat;
+			this.next = next;
+		}
 	}
 
-	public Node head;
+	public Node head = null;
 	public int cant = 0;
 
 	public boolean isEmpty(){
-		return false;
+		return cant == 0;
 	}
 
 	public void put(String dat){
-
+		if(head == null){
+			head = new Node(dat, null);
+		}else{
+			Node aux = new Node(dat, head);
+			head = aux;
+		}
 	}
 
 	public void add(String dat, int index){
@@ -35,6 +45,17 @@ class LinkedList {
 
 	public void delete(String dat){
 	
+	}
+
+	public String toString(){
+		String out = "";
+		Node aux = head;
+		
+		while(aux != null){
+			out += aux.toString()+" - ";
+			aux = aux.next;
+		}
+		return out;
 	}
 
 }
