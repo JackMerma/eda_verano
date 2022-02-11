9f4c5129db4658417047d58545cc81b1a1c7d8fa
Jackson Merma <jmermap@unsa.edu.pe>
2022-02-10 20:23:39 -0500

    metodo isEmpty, add, search implementadas...y uso de Comparable para T

--- ejemplos/listas/ArregloList.java
+++ ejemplos/listas/ArregloList.java
@@ -7,10 +7,11 @@
 
 import java.util.*;
 
-public class ArregloList<T>{
+public class ArregloList<T extends Comparable<T>>{
 
 	private T[] list;
 	private int total;
+	private int added = 0;
 
 	public ArregloList(int n){
 		//cast necesario
@@ -21,12 +22,18 @@ public class ArregloList<T>{
 	//principales metodos
 	
 	public boolean isEmpty(){
+		return added == 0;
 	}
 
 	public void add(T dat, int index){
+		list[index] = dat;
 	}
 	
 	public int search(T dat){
+		for(int i = 0; i < this.total; i++)
+			if(list[i] != null && this.list[i].compareTo(dat) == 0)
+				return i;
+		return -1;
 	}
 
 	public T get(int index){
