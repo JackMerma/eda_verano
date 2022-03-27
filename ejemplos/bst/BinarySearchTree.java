e0300709909c8d49d8ad09ae5d3c4bcd5dd1bd8f
Jackson Merma <jmermap@unsa.edu.pe>
2022-03-26 19:52:20 -0500

    plantilla base de un bst

---
+++ ejemplos/bst/BinarySearchTree.java
@@ -0,0 +1,63 @@
+/**
+ * @author      : Jackson Merma (mermaportocarreroj@gmail.com)
+ * @created     : 26/03/2022
+ * @filename    : BinarySearchTree
+ */
+public class BinarySearchTree <T extends Comparable<T>>{
+
+	class Node<T extends Comparable<T>>{
+		T data;
+		Node right;
+		Node left;
+		public Node(T dat, Node r, Node l){
+			data = dat;
+			right = r;
+			left = l;
+		}
+	}
+
+	//methods
+	public void add(T dat){
+
+	}
+
+	public T search (T dat){
+		
+	}
+
+	public void delete(T dat){
+
+	}
+
+	//recorridos
+	public String inOrden(){
+
+	}
+
+	public String preOrden(){
+
+	}
+
+	public String postOrden(){
+
+	}
+
+	//retos
+	public String searchLessThan(T dat){
+
+	}
+
+	public String searchGreaterThan(T dat){
+
+	}
+
+	public T searchAtPosition(int k){
+
+	}
+
+	public static void main(String [] args){
+		//code
+	}
+}
+
+
